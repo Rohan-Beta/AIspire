7645dd9236431f374ddb64d9956cd4eff7f5d24c
Rohan-Beta <rohitraha952@gmail.com>
2025-06-20 18:33:44 +0530

    feat: update not found page UI

---
+++ app/not-found.tsx
@@ -0,0 +1,12 @@
+// app/not-found.tsx
+import NotFound from "../components/NotFound";
+
+export default function NotFoundPage() {
+  return (
+    <html lang="en">
+      <body className="overflow-hidden bg-black">
+        <NotFound />
+      </body>
+    </html>
+  );
+}
